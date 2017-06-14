5ce9c841f59a849e0ea9b4643f45b8afcf34f22b
Thomas Burke <tburke@tb99.com>
2017-06-14 10:36:48 -0500

    gps notes.

--- showkts.go
+++ showkts.go
@@ -183,6 +183,7 @@ func main() {
 }
 
 /*
+http://www.catb.org/gpsd/installation.html
 https://github.com/tarm/serial/blob/master/serial_linux.go
 Set baud
 cflagToUse := syscall.CREAD | syscall.CLOCAL | syscall.B4800
